d98d9a644dee617313b86ade65f21cabf7dc667b
Amalmalik Abuzarov <amalmalikredrobin@gmail.com>
2021-12-13 12:31:58 +0500

    my last commit

--- task11122021.py
+++ task11122021.py
@@ -1,6 +1,7 @@
 def format_tel(tel):
-    for tel in range(4):
-        return tel.append('(')
+    return f'{tel[0:4]}({tel[4:6]) {tel[6:9]}}-{tel[9:11]}-{tel[11:13]}'
     
+def format_tel(tel):
+    return '{}{}{}{}({}{}) {}{}{}-{}-{}'.format(*list(tel))
 
 print(format_tel('+998996116661'))
